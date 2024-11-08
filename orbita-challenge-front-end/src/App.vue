2327b257ff51a7164b1f953e6c1b90922fbb8d11
Ian Hadrien <ianhadrien@gmail.com>
2024-11-08 17:25:00 -0300

    Initial commit

---
+++ orbita-challenge-front-end/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-main>
+      <!-- <router-view /> -->
+       <div>
+        Teste
+       </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
