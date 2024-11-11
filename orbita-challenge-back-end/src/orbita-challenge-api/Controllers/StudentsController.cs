2c08329dd98523e49f5c895372935f3ca0a63c9e
Ian Hadrien <ianhadrien@gmail.com>
2024-11-11 00:07:38 -0300

    feat: add student controller

--- orbita-challenge-back-end/src/orbita-challenge-api/Controllers/StudentsController.cs
+++ orbita-challenge-back-end/src/orbita-challenge-api/Controllers/StudentsController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using orbita_challenge.Application.UseCases.Students.GetAll;
+using orbita_challenge.Application.UseCases.Students.GetById;
 using orbita_challenge.Application.UseCases.Students.Register;
 using orbita_challenge.Communication.Requests;
+using orbita_challenge.Communication.Responses;
 
 namespace orbita_challenge_api.Controllers;
 
@@ -9,19 +12,38 @@ namespace orbita_challenge_api.Controllers;
 public class StudentsController : ControllerBase
 {
     [HttpPost]
-    public IActionResult Register([FromBody] RequestRegisterStundetJson request)
+    [ProducesResponseType(typeof(ResponseRegisteredStudentJson), StatusCodes.Status201Created)]
+    public async Task<IActionResult> Register(
+        [FromServices] IRegisterStudentUseCase useCase,
+        [FromBody] RequestRegisterStundetJson request
+     )
     {
-        try
-        {
-            var useCase = new RegisterStudentUseCase();
+        var response = await useCase.Execute(request);
 
-            var response = useCase.Execute(request);
+        return Created(string.Empty, response);
+    }
 
-            return Created(string.Empty, response);
-        } 
-        catch (ArgumentException ex)
-        {
-            return BadRequest(ex.Message);
-        }
+    [HttpGet]
+    [ProducesResponseType(typeof(ResponseStudentsJson), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAllStudents([FromServices] IGetAllStudentsUseCase useCase)
+    {
+        var response = await useCase.Execute();
+
+        if (response.Students.Count != 0)
+            return Ok(response);
+
+        return NoContent();
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    [ProducesResponseType(typeof(ResponseOneStudentJson), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetById(
+        [FromServices] IGetStudentByIdUseCase useCase,
+        [FromRoute] long id)
+    {
+        var response = await useCase.Execute(id);
+
+        return Ok(response);
     }
 }
