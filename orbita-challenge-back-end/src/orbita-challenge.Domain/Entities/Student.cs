14fa4bed096413a6b6504cbe3f53202b28f97d20
Ian Hadrien <ianhadrien@gmail.com>
2024-11-11 00:06:10 -0300

    feat: add entitie Student

---
+++ orbita-challenge-back-end/src/orbita-challenge.Domain/Entities/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace orbita_challenge.Domain.Entities;
+public class Student
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; }
+    public string Ra { get; set; }
+    public string Cpf { get; set; }
+}
