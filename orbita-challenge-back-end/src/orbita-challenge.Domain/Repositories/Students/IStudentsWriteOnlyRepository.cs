44ae0376f05c93c5d1db4a8a19599ffef23dcf48
Ian Hadrien <ianhadrien@gmail.com>
2024-11-11 00:10:37 -0300

    feat: add repository student

---
+++ orbita-challenge-back-end/src/orbita-challenge.Domain/Repositories/Students/IStudentsWriteOnlyRepository.cs
@@ -0,0 +1,7 @@
+﻿using orbita_challenge.Domain.Entities;
+
+namespace orbita_challenge.Domain.Repositories.Students;
+public interface IStudentsWriteOnlyRepository
+{
+    Task Add(Student student);
+}
---
+++ orbita-challenge-back-end/src/orbita-challenge.Infrastructure/DataAccess/Repositories/StudentsRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using orbita_challenge.Domain.Entities;
+using orbita_challenge.Domain.Repositories.Students;
+
+namespace orbita_challenge.Infrastructure.DataAccess.Repositories;
+internal class StudentsRepository : IStudentsReadOnlyRepository, IStudentsWriteOnlyRepository
+{
+    private readonly OrbitaChallengeDbContext _dbContext;
+    public StudentsRepository(OrbitaChallengeDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Add(Student student)
+    {
+        await _dbContext.Students.AddAsync(student);
+    }
+
+    public async Task<List<Student>> GetAll()
+    {
+        return await _dbContext.Students.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<Student?> GetById(long id)
+    {
+        return await _dbContext.Students.AsNoTracking().FirstOrDefaultAsync(student => student.Id == id);
+    }
+}
