4b32364a34b90e94f31abdef2071757060442595
Ian Hadrien <ianhadrien@gmail.com>
2024-11-11 00:04:47 -0300

    feat: add add AutoMapper package

---
+++ orbita-challenge-back-end/src/orbita-challenge.Application/AutoMapper/AutoMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using orbita_challenge.Communication.Requests;
+using orbita_challenge.Communication.Responses;
+using orbita_challenge.Domain.Entities;
+
+namespace orbita_challenge.Application.AutoMapper;
+public class AutoMapping : Profile
+{
+    public AutoMapping()
+    {
+        RequestToEntity();
+        EntityToResponse();
+    }
+    private void RequestToEntity()
+    {
+        CreateMap<RequestRegisterStundetJson, Student>();
+    }
+    private void EntityToResponse()
+    {
+        CreateMap<Student, ResponseRegisteredStudentJson>();
+        CreateMap<Student, ResponseOneStudentJson>();
+    }
+}
