4a7f88edcde3ca7b62f81470016f7d851f8ffe0d
Ian Hadrien <ianhadrien@gmail.com>
2024-11-11 00:09:58 -0300

    feat: add test register student

---
+++ orbita-challenge-back-end/tests/Validators.Tests/Students/RegisterStudentValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using orbita_challenge.Application.UseCases.Students.Register;
+using orbita_challenge.Communication.Requests;
+
+namespace Validators.Tests.Students;
+public class RegisterStudentValidatorTests
+{
+    [Fact]
+    public void Sucess()
+    {
+        // Arrange
+        var request = new RequestRegisterStundetJson
+        {
+            Nome = "ian",
+            Email = "test",
+            RA = "123",
+            CPF = "12312312312",
+        };
+
+        // Act
+        var useCase = new RegisterStudentUseCase();
+        var result = useCase.Execute(request);
+
+        // Assert
+        Assert.NotNull(result);
+        //Assert.True(result.Success);
+        Assert.Equal("ian", result.Nome);
+    }
+}
