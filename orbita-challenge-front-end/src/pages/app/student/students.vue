1c52098c7f32a57ee68e09de20bb703804a0659d
Ian Hadrien <ianhadrien@gmail.com>
2024-11-12 00:21:02 -0300

    refactor: removing unused code from the students page

--- orbita-challenge-front-end/src/pages/app/student/students.vue
+++ orbita-challenge-front-end/src/pages/app/student/students.vue
@@ -16,95 +16,45 @@
     <v-data-table
       :headers="headers"
       :items="desserts"
-      :sort-by="[{ key: 'calories', order: 'asc' }]"
+      :sort-by="[{ key: 'id', order: 'asc' }]"
+      :fixed-footer="{
+        itemsPerPageText: 'Itens por página',
+        pageText: (pageStart, pageStop, itemsLength) => 
+          `${pageStart}-${pageStop} de ${itemsLength}`
+      }"
     >
-      <template v-slot:top>     
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue-darken-1" variant="text" @click="closeDelete">Cancel</v-btn>
-              <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">OK</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </template>
-
       <template v-slot:item.actions="{ item }">
-        <v-icon
-          class="me-2"
-          size="small"
-          @click="editItem(item)"
-        >
-          mdi-pencil
-        </v-icon>
-        <v-icon
-          size="small"
-          @click="deleteItem(item)"
-        >
-          mdi-delete
-        </v-icon>
+        <UpdateStudent :student="item" />
+
+        <DeleteStudent :student="item" />
       </template>
     </v-data-table>
   </div>
 </template>
 
 <script lang="ts">
-  import RegisterStudent from './register-student.vue';
+  import RegisterStudent from './register-student.vue'
+  import UpdateStudent from './update-student.vue'
+  import DeleteStudent from './delete-student.vue'
 
   export default {
     name: "Students",
 
     data: () => ({
-      dialog: false,
-      dialogDelete: false,
-
       headers: [
-        { title: 'Registro Acadêmico', align: 'start', sortable: false, key: 'name'},
-        { title: 'Nome', key: 'calories' },
-        { title: 'CPF', key: 'fat' },
-        { title: '', key: 'actions', sortable: false },
+        { title: 'REGISTRO ACADÊMICO', align: 'start', sortable: false, key: 'ra'},
+        { title: 'NOME', key: 'name' },
+        { title: 'CPF', key: 'cpf' },
+        { title: '', key: 'actions', align: 'center', sortable: false },
       ],
 
       desserts: [],
-      editedIndex: -1,
-
-      editedItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
-      },
-
-      defaultItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
-      },
     }),
 
     components: {
-      RegisterStudent
-    },
-
-    computed: {
-      formTitle () {
-        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
-      },
-    },
-
-    watch: {
-      dialog (val) {
-        val || this.close()
-      },
-      dialogDelete (val) {
-        val || this.closeDelete()
-      },
+      RegisterStudent,
+      UpdateStudent,
+      DeleteStudent
     },
 
     created () {
@@ -115,61 +65,21 @@
       initialize () {
         this.desserts = [
           {
-            name: '12312312323',
-            calories: 'Ian Hadrien Santos Ferreira',
-            fat: 12312312323,
+            id: '123123',
+            name: 'teste 1',
+            email: 'Ianteste@enmai.com',
+            ra: '12312312323',
+            cpf: '12312312323',
           },
           {
-            name: '12312312323',
-            calories: 'Ian Hadrien Santos Ferreira',
-            fat: 12312312323,
+            id: '123123',
+            name: 'teste 2',
+            email: 'Ianteste2@enmai.com',
+            ra: '12312312113',
+            cpf: '12312312323',
           },
         ]
-      },
-
-      editItem (item) {
-        console.log("Edited")
-        // this.editedIndex = this.desserts.indexOf(item)
-        // this.editedItem = Object.assign({}, item)
-        // this.dialog = true
-      },
-
-      deleteItem (item) {
-        // this.editedIndex = this.desserts.indexOf(item)
-        // this.editedItem = Object.assign({}, item)
-        // this.dialogDelete = true
-        console.log("deleteItem")
-      },
-
-      deleteItemConfirm () {
-        this.desserts.splice(this.editedIndex, 1)
-        this.closeDelete()
-      },
-
-      close () {
-        this.dialog = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
-
-      closeDelete () {
-        this.dialogDelete = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
-
-      save () {
-        if (this.editedIndex > -1) {
-          Object.assign(this.desserts[this.editedIndex], this.editedItem)
-        } else {
-          this.desserts.push(this.editedItem)
-        }
-        this.close()
-      },
+      }
     },
   }
 </script>
