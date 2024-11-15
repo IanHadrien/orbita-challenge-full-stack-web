09d39d1a59ac65e9fb533f2104e79e078177e25b
Ian Hadrien <ianhadrien@gmail.com>
2024-11-15 01:32:03 -0300

    refactor: request update and register student

--- orbita-challenge-back-end/src/orbita-challenge.Communication/Requests/RequestRegisterStundetJson.cs
+++ orbita-challenge-back-end/src/orbita-challenge.Communication/Requests/RequestRegisterStundetJson.cs
@@ -5,7 +5,7 @@ namespace orbita_challenge.Communication.Requests;
 public class RequestRegisterStundetJson
 {
     [Required(ErrorMessage = "O campo Nome é obrigatório.")]
-    [StringLength(100, MinimumLength = 3, ErrorMessage = "O Nome deve ter entre 3 e 100 caracteres.")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "O Nome deve ter mais de 3 caracteres.")]
     public string Name { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "O campo Email é obrigatório.")]
@@ -13,10 +13,10 @@ public class RequestRegisterStundetJson
     public string Email { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "O campo RA é obrigatório.")]
-    [StringLength(20, MinimumLength = 3, ErrorMessage = "O RA deve ter entre 3 e 20 caracteres.")]
+    [StringLength(6, MinimumLength = 6, ErrorMessage = "O RA deve ter 6 caracteres.")]
     public string RA { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "O campo CPF é obrigatório.")]
-    [StringLength(14, MinimumLength = 11, ErrorMessage = "O CPF deve ter entre 11 e 14 caracteres.")]
+    [StringLength(11, MinimumLength = 11, ErrorMessage = "O CPF deve ter 11 caracteres.")]
     public string CPF { get; set; } = string.Empty;
 }
--- orbita-challenge-back-end/src/orbita-challenge.Communication/Requests/RequestUpdateStudentJson.cs
+++ orbita-challenge-back-end/src/orbita-challenge.Communication/Requests/RequestUpdateStudentJson.cs
@@ -4,7 +4,7 @@ namespace orbita_challenge.Communication.Requests;
 public class RequestUpdateStudentJson
 {
     [Required(ErrorMessage = "O campo Nome é obrigatório.")]
-    [StringLength(100, MinimumLength = 3, ErrorMessage = "O Nome deve ter entre 3 e 100 caracteres.")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "O Nome deve ter mais de 3 caracteres.")]
     public string Name { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "O campo Email é obrigatório.")]
